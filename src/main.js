4c9ad92ec83b40349c5ef82d1c8ba639cd7df7c5
WilsonCorrea <wilzon01@gmail.com>
2022-10-24 22:44:55 -0500

    En index (add. boton de logout y nuevas clases a los botones nav) en main.js(add. importe de funciones)

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
-// import './app/firebase.js'
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-auth.js"
+import { auth } from './app/firebase.js'
 // console.log('Hello world');
 import './app/signupForm.js'
+
+onAuthStateChanged(auth, async (user) => {
+    console.log(user)
+});
