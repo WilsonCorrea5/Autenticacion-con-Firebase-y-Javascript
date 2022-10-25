81efac9da5da252b575fecc08eecffd0f2ce4b49
WilsonCorrea <wilzon01@gmail.com>
2022-10-24 22:57:13 -0500

    En main.js(add . importe del archivo logout.js)

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.12.1/fi
 import { auth } from './app/firebase.js'
 // console.log('Hello world');
 import './app/signupForm.js'
+import './app/logout.js'
 
 onAuthStateChanged(auth, async (user) => {
     console.log(user)
