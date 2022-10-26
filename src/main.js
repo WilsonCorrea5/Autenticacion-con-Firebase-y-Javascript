4eb1cdd1950ff769b019447ee2c21eca78eaef56
WilsonCorrea <wilzon01@gmail.com>
2022-10-25 23:16:55 -0500

    En main.js(add. importamos nuevo archivo signinForm

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-auth.js"
 import { auth } from './app/firebase.js'
 import { loginCheck } from "./app/loginCheck.js"
-// console.log('Hello world');
+
+
 import './app/signupForm.js'
 import './app/logout.js'
-import './app/loginCheck.js'
+import './app/signinForm.js'
 
 onAuthStateChanged(auth, async (user) => {
 
