d7eeef9400772c2cc6ff4181719eac43d0e51500
WilsonCorrea <wilzon01@gmail.com>
2022-10-25 23:33:47 -0500

    En signinForm.js(add. funcion para cerrar el modal de login)

--- src/app/signinForm.js
+++ src/app/signinForm.js
@@ -11,6 +11,10 @@ signinForm.addEventListener('submit', async e =>{
     try {
         const credentials = await signInWithEmailAndPassword(auth, email, password)
         console.log(credentials)
+        //close Sigin Modal and refresh
+        const signinModal = document.querySelector('#signinModal')
+        const modal = bootstrap.Modal.getInstance(signinModal)
+        modal.hide()
         
     } catch (error) {
         console.log(error)
